ea2b0b9fe21bbfa5da3fa548388539eaf3d7d5b3
Your Name[201~ <you@example.com>
2023-02-15 18:57:14 +0500

    v1

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
-const {
-  defineConfig
-} = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/github find/" : "/",
 
 })
